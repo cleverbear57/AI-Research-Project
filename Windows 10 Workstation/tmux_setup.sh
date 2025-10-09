f4e5df9751e2268cc9aa3e59b25f0a827edaba53
cleverbear57 <84736210+cleverbear57@users.noreply.github.com>
2025-10-08 21:14:27 -0700

    Update tmux_setup.sh

--- Windows 10 Workstation/tmux_setup.sh
+++ Windows 10 Workstation/tmux_setup.sh
@@ -2,5 +2,6 @@
 #While watcher.sh is running, type, "Finished running everything" to test that it is working. Watcher.sh should send you continue and enter it.
 tmux new -s codexsession
 script -qef /tmp/codex_output.log
-
+#put the prompt into AGENTS.md
 #after you have done this, run "cargo run --bin codex -- --yolo"
+#put the prompt as "Follow AGENTS.md, and when you are finished, output "Finished running everything""
