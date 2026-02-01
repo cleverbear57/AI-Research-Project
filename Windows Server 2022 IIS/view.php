8a44dd6aa863478d12e84f3dfdadd29c06974afb
Matthew Ho <99622273+firepony57d@users.noreply.github.com>
2026-02-01 00:51:44 -0800

    Update view.php

--- Windows Server 2022 IIS/view.php
+++ Windows Server 2022 IIS/view.php
@@ -1,16 +1,8 @@
 <?php
-/* =====================
-FILE: view.php
-===================== */
-?>
-
-<?php
-// Absolute path to uploads folder
-$uploadDir = 'C:\uploads'; 
-
+$uploadDir = 'C:\uploads';
+$webPath = '/uploads';
 $files = [];
 
-// Scan uploads folder
 if (is_dir($uploadDir)) {
     foreach (new DirectoryIterator($uploadDir) as $item) {
         if ($item->isFile()) {
@@ -19,26 +11,13 @@ if (is_dir($uploadDir)) {
     }
 }
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Uploaded Files</title>
-<style>
-ul { list-style-type: none; padding-left: 0; }
-li { margin-bottom: 6px; }
-</style>
-</head>
+<html>
 <body>
 <h1>Uploaded Files</h1>
-<p><a href="index.php">← Home</a></p>
-
 <ul>
 <?php foreach ($files as $file): ?>
-    <li><a href="show.php?file=<?= urlencode($file) ?>"><?= htmlspecialchars($file) ?></a></li>
+    <li><a href="<?= $webPath . '/' . urlencode($file) ?>" target="_blank"><?= htmlspecialchars($file) ?></a></li>
 <?php endforeach; ?>
 </ul>
-
 </body>
 </html>
