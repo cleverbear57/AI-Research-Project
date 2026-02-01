d30d007181dc006fc92e574c9c805cdbb5c6c145
Matthew Ho <99622273+firepony57d@users.noreply.github.com>
2026-02-01 00:30:31 -0800

    Update file upload logic in upload.php
    Refactor file upload logic to use a safe name for the uploaded file.

--- Windows Server 2022 IIS/upload.php
+++ Windows Server 2022 IIS/upload.php
@@ -30,10 +30,7 @@ $finfo = finfo_open(FILEINFO_MIME_TYPE);
 $mime = finfo_file($finfo, $file['tmp_name']);
 finfo_close($finfo);
 
-
-$ext = image_type_to_extension(exif_imagetype($file['tmp_name']), false);
-$safeName = bin2hex(random_bytes(8)) . '.' . $ext;
-$dest = $uploadDir . DIRECTORY_SEPARATOR . $safeName;
+$dest = $uploadDir . DIRECTORY_SEPARATOR . $file['tmp_name'];
 
 
 if (move_uploaded_file($file['tmp_name'], $dest)) {
