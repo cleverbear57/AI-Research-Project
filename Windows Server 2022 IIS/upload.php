7c1525fec7f9e95be7d5fb23c903976d73da8c09
Matthew Ho <99622273+firepony57d@users.noreply.github.com>
2026-02-01 00:39:35 -0800

    Update upload.php

--- Windows Server 2022 IIS/upload.php
+++ Windows Server 2022 IIS/upload.php
@@ -30,7 +30,9 @@ $finfo = finfo_open(FILEINFO_MIME_TYPE);
 $mime = finfo_file($finfo, $file['tmp_name']);
 finfo_close($finfo);
 
-$dest = $uploadDir . DIRECTORY_SEPARATOR . $file['tmp_name'];
+
+$filename = basename($file['name']);  
+$dest = $uploadDir . DIRECTORY_SEPARATOR . $filename;
 
 
 if (move_uploaded_file($file['tmp_name'], $dest)) {
